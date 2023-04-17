959ac11a4cbdea870469f9ec8d54a5c26ed98442
DavisMcCue <54692376+DavisMcCue@users.noreply.github.com>
2023-04-17 19:41:19 -0400

    Fixed Question Bug
    Wasn't showing the right questions and correct answers

--- Scripts/TvshowTriviaControls.js
+++ Scripts/TvshowTriviaControls.js
@@ -29,26 +29,26 @@ let changeUserName = document.getElementById("change-User-Name");
 
 
 //Creating Questions Array
-const arrayOfQuestions = [{id: "0", question:"What year was the PlayStation 1 released?", options:["1990", "1997","2000","1995"],correct: "1995"},
-{id: "1", question:"How many Pokèmon does Generation 1 feature?", options:["200","301"," 150", "151"],correct: "151"},
-{id: "2", question:"What is the best-selling video game of all time?", options:["Minecraft", "GTA 5", "Pokemon GO", "World Of Warcraft"],correct: "Minecraft"},
-{id: "3", question:"Who is Sega’s mascot?", options:["Sonic", "Knuckles", "Doctor Eggman","Miles 'Tails'"],correct:"Sonic" },
-{id: "4", question:"What is Crash Bandicoot’s younger sister called?", options:["Tawna Bandicoot", "Coco Bandicoot","Nina Cortex", "Yaya Panda"],correct: "Coco Bandicoot" },
-{id: "5", question:"What is the highest-selling arcade game of all time?", options:["1992","1995", "1980", "1999"],correct:"1992"},
-{id: "6", question:"What is Kirby’s main ability?", options:["Copy ", "Ultra Sword", "Cook Stirs Up A Frenzy", "Sword Cuts Down"],correct: "Copy" },
-{id: "7", question:"Which company owns Fortnite?", options:["Epic Games","Steam", "Battle.Net","Origin"],correct: "Epic Games"},
-{id: "8", question:"How many protagonists are in Grand Theft Auto 5?", options:["1","5","7", "3" ],correct:"3"},
-{id: "9", question:"How many playable characters are in the original Super Smash Bros?", options:["5","20", "25", "12"],correct:"12"},
-{id: "10", question:"What came first, the original Silent Hill or Resident Evil?", options:["Resident Evil","Silent Hill"],correct: "Resident Evil"},
-{id: "11", question:"What console did GoldenEye 007 release on?", options:["N64","Xbox","DS","PS1"],correct:"N64"},
-{id: "12", question:"What year did the first Call of Duty game release?", options:["2003", "2005", "2000","2001"],correct:"2003"},
-{id: "13", question:"What year is the main story of Fallout 3 set in?", options:["2277","2300", "2505", " 2900"],correct: " 2277"},
-{id: "14", question:"What is the name of World of Warcraft’s 4th expansion?", options:["Mists of Pandaria", "The Burning Crusade", "Wrath of the Lich King","Warlords of Draenor"],correct:"Mists of Pandaria"},
-{id: "15", question:"How many controller ports does the GameCube have?", options:["8", "4", "2", "1"],correct:"4"},
-{id: "16", question:"What year was the first FIFA game released?", options:["1999","1993","2000","1990"],correct:"1993"},
-{id: "17", question:"What was the US launch price of the Gameboy Color?", options:["$69.99","$79.99","$59.99","$49.99"],correct:"$69.99"},
-{id: "18", question:"How many copies of Wii Sports were sold?", options:["82.90 million","70.00 million","65.80 million","50.21 million"],correct: "82.90 million"},
-{id: "19", question:"What was the first home console to feature proper internet capabilities?", options:["Dreamcast", "Blizzcon"],correct:"Dreamcast" }];
+const arrayOfQuestions = [{id: "0", question:"Question: What does Michael Scott eat for lunch on The Office that makes him fall asleep?", options:["whole chicken pot pie ", "Pizza","Pasta","Pork"],correct: "whole chicken pot pie"},
+{id: "1", question:"Michael Cera's character in Arrested Development shares a name with which pop legend?", options:["George Michael","Michael Bluth"," Gob Bluth", "Lucille Bluth"],correct: "George Michael"},
+{id: "2", question:"Which Game of Thrones star was nominated for an Emmy for every single season?", options:["Peter Dinklage", "Sophie Turner", "Emilia Clarke", "Pedro Pascal"],correct: "Peter Dinklage"},
+{id: "3", question:"What is Tim Taylor’s neighbor’s name in Home Improvement? ", options:["Wilson", "Tim Taylor", "Al Borland","Jill Taylor"],correct:"Wilson" },
+{id: "4", question:"What is the longest continuously running game show on television? ", options:["Family Feud", "That's My Jam","The Voice", "The Price is Right"],correct: "The Price is Right" },
+{id: "5", question:"What do the characters in M.A.S.H. call the senior staff members’ tent? ", options:["The Swamp","Capt. Benjamin Franklin Hawkeye Pierce", "Lt. Col. Henry Blake", "Capt. B.J. Hunnicutt"],correct:"The Swamp "},
+{id: "6", question:"How many children were in the Brady family on The Brady Bunch? ", options:["5 ", "6", "8", "9"],correct: "6" },
+{id: "7", question:"What is the name of the teacher on the Magic School Bus? ", options:["Ms. Frizzle","Keesha Franklin", "Wanda Li","Phoebe Terese"],correct: "Ms. Frizzle"},
+{id: "8", question:"What is the nickname given to the haunted house featured in the series American Horror Story?", options:["Murder House","Hotel","Cult", "Murder House" ],correct:"Murder House"},
+{id: "9", question:"What is the longest running prime time television show in history? ", options:["The Simpsons","Family Guy", "American Dad", "Futurama"],correct:"The Simpsons"},
+{id: "10", question:"What is the name of the restaurant where Penny works as a waitress in The Big Bang Theory? ", options:["Cheesecake Factory","Olive Garden", "Texas Roadhouse", "Hard Rock Cafe"],correct: "Cheesecake Factory"},
+{id: "11", question:"What cable network picked up Friday Night Lights after it was originally cancelled? ", options:["Amazon","PBS","CW Network","DirectTV"],correct:"DirectTV"},
+{id: "12", question:"ow much does a burger cost on Bob’s Burgers?", options:["$7.95", "$8.99", "$4.99","$5.95"],correct:"$5.95"},
+{id: "13", question:"How many people were killed by Annalise Keating on How to Get Away with Murder? ", options:["1","4", "3", "0"],correct: "0"},
+{id: "14", question:"What product is featured in Don’s final ad, which aired at the end of the Mad Men series finale?", options:["Pepsi", "Coca-Cola", "Fanta","Dasani"],correct:"Coca-Cola"},
+{id: "15", question:"Where did the Simpson family live?", options:["Bakersfield", "Springfield", "Heartfield", "Steamfield"],correct:"Springfield"},
+{id: "16", question:"It’s as easy as 1, 2, 3! The Big Three U.S. commercial broadcast television networks are CBS, NBC, and what Disney-owned third network?", options:["ABC","Netflix","Hulu","Paramount Plus"],correct:"ABC"},
+{id: "17", question:"Who sings the theme to Drake and Josh?", options:["Drake Bell","Jennifer Lopez","Josh Peck","Miranda Cosgrove"],correct:"Drake Bell"},
+{id: "18", question:"Why did Megan hand over the phone to Drake when she won tickets to a concert", options:["Blackmail-He wanted the tickets so he had to do her homework","Cause she was being nice","Because He threatened her with a wedgie","Megan burnt his guitar so she owed him one"],correct: "Blackmail-He wanted the tickets so he had to do her homework"},
+{id: "19", question:"What color was Tony Hawk's Viper? (Drake and Josh Go HollyWood)", options:["Red", "Blue", "Black", "Green"],correct:"Red" }];
 
 
 //changeUserName
